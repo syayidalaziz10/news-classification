f14a9986563658e03caccbb35d997c3e21550b8b
syayidalaziz10 <shorashiro15@gmail.com>
2023-12-10 09:37:47 +0700

    change layout

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ from generate_label import get_label
 def main():
 
     st.set_page_config(
-        page_title="Aplikasi Kategori Berita | Klasifikasi Berita CNN Indonesia", page_icon="📖")
+        page_title="Aplikasi Kategori Berita | Klasifikasi Berita CNN Indonesia", page_icon="📺")
 
-    st.image("assets/banner.png", use_column_width=True)
-    st.subheader("News Classification: Aplikasi Kategori untuk Berita")
-    st.caption("Berita umumnya dikategorikan menjadi beberapa jenis kategori seperti olahraga, ekonomi, hiburan dan kategori lainnya. Dengan news classification ini kita dapat menemukan jenis kategori berita yang sesuai dengan isi berita tersebut.")
+    col1, col2 = st.columns(2)
+
+    with col1:
+
+        st.image("assets/banner.png", use_column_width=True)
+
+    with col2:
+        st.subheader("News Classification: Aplikasi Kategori untuk Berita")
+        st.caption("Berita umumnya dikategorikan menjadi beberapa jenis kategori seperti olahraga, ekonomi, hiburan dan kategori lainnya. Dengan news classification ini kita dapat menemukan jenis kategori berita yang sesuai dengan isi berita tersebut.")
 
     news_text = st.text_area(
         "Masukkan Isi Berita", key="input_text", height=250)
